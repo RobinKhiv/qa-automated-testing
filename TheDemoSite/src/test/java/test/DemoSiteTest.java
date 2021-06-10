ac0969e39fb181c6dbbdcf453dfc2cb505a8734a
Robin Khiv <robinkhiv@gmail.com>
2021-06-10 11:51:33 -0700

    rk:added methods for demositetest

--- TheDemoSite/src/test/java/test/DemoSiteTest.java
+++ TheDemoSite/src/test/java/test/DemoSiteTest.java
@@ -1,5 +1,32 @@
 package test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
 public class DemoSiteTest {
 
+	static WebDriver driver;
+	
+	@BeforeAll
+	public static void init() {
+		
+	}
+	
+	@BeforeEach
+	public void setup() {
+		
+	}
+	
+	@AfterAll
+	public static void teardown() {
+		
+	}
+	
+	@Test
+	public void test() {
+		
+	}
 }
