efc23d9e5c81b5834a57bb128341776ca5f2167c
Robin Khiv <robinkhiv@gmail.com>
2021-06-11 06:24:11 -0700

    rk:added class to enter username and password

--- TheDemoSite/src/test/java/pages/AddUser.java
+++ TheDemoSite/src/test/java/pages/AddUser.java
@@ -1,5 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AddUser {
 
+	@FindBy(xpath = "//tbody/tr[1]/td[2]/p[1]/input[1]")
+	private WebElement userInput;
+
+	@FindBy(xpath = "//tbody/tr[1]/td[2]/p[1]/input[1]")
+	private WebElement passInput;
+
+	public void signUp(String username, String password) {
+		userInput.sendKeys(username);
+		passInput.sendKeys(password);
+		passInput.submit();
+	}
 }
