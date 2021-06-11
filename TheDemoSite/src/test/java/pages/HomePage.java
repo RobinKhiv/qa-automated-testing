77d2add8861b090e9c6db1baa83e2d0f750d6463
Robin Khiv <robinkhiv@gmail.com>
2021-06-11 06:25:08 -0700

    rk:added method to click links

--- TheDemoSite/src/test/java/pages/HomePage.java
+++ TheDemoSite/src/test/java/pages/HomePage.java
@@ -1,11 +1,27 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
 
 	private static final String URL = "http://thedemosite.co.uk/";
 
+	@FindBy(linkText="3. Add a User")
+	private WebElement addUser;
+	
+	@FindBy(linkText="4. Login")
+	private WebElement login;
+	
 	public static String getURL() {
 		return URL;
 	}
 	
+	public void navAddUser() {
+		addUser.click();
+	}
+	
+	public void navLogin() {
+		login.click();
+	}
 }
