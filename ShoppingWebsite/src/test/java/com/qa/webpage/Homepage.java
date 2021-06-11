c44ad463b921eabbf85bcabba51728c03478f6e8
Robin Khiv <robinkhiv@gmail.com>
2021-06-11 08:38:18 -0700

    rk:added dress content navigation

--- ShoppingWebsite/src/test/java/com/qa/webpage/Homepage.java
+++ ShoppingWebsite/src/test/java/com/qa/webpage/Homepage.java
@@ -1,11 +1,20 @@
 package com.qa.webpage;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Homepage {
 
 	private final static String URL = "http://automationpractice.com/index.php";
 
+	@FindBy(linkText = "Dresses")
+	private WebElement dresses;
+	
 	public static String getUrl() {
 		return URL;
 	} 
 
+	public void navDresses() {
+		dresses.click();
+	}
 }
