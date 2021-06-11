d718c6efdcc8f968b86c5517b18c1f312aae8412
Robin Khiv <robinkhiv@gmail.com>
2021-06-11 08:39:26 -0700

    rk:added method to test searchkey

--- ShoppingWebsite/src/test/java/com/qa/test/WebsiteTest.java
+++ ShoppingWebsite/src/test/java/com/qa/test/WebsiteTest.java
@@ -1,27 +1,36 @@
 package com.qa.test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import com.qa.webpage.Homepage;
+import com.qa.webpage.Searchbar;
 
 public class WebsiteTest {
-	
+
 	static WebDriver driver;
-	
-	@BeforeAll 
+
+	@BeforeAll
 	public static void init() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().window().fullscreen();
+		driver.manage().window().setSize(new Dimension(1366, 768));
 	}
-	
+
 	@BeforeEach
 	public void setup() {
 		try {
@@ -31,10 +40,27 @@ public class WebsiteTest {
 			System.out.println("Failed to load homepage");
 		}
 	}
-	
+
 	@AfterAll
 	public static void teardown() {
 		driver.quit();
 	}
-	
+
+	@Test
+	public void test() throws InterruptedException {
+		String searchkey = "dress";
+		Searchbar search = PageFactory.initElements(driver, Searchbar.class);
+		
+		search.find(searchkey);
+		Thread.sleep(3000);
+		
+		List<WebElement> products = driver.findElements(By.className("ajax_block_product"));
+		for (WebElement product : products) {
+			String productName = product.findElement(By.className("product-name")).getText().toLowerCase();
+
+			if (!productName.contains(searchkey)) {
+				fail("Non-relevant item returned");
+			}
+		}
+	}
 }
