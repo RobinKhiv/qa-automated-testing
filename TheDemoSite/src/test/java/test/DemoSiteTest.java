04e7ab10716bcaddf8872efda50ab7ea43bfc062
Robin Khiv <robinkhiv@gmail.com>
2021-06-11 06:49:16 -0700

    rk:added method to check for sucessful login

--- TheDemoSite/src/test/java/test/DemoSiteTest.java
+++ TheDemoSite/src/test/java/test/DemoSiteTest.java
@@ -1,28 +1,33 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
+import pages.AddUser;
 import pages.HomePage;
 
 public class DemoSiteTest {
 
 	static WebDriver driver;
-	
+
 	@BeforeAll
 	public static void init() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().fullscreen();
 	}
-	
+
 	@BeforeEach
 	public void setup() {
 		try {
@@ -32,14 +37,27 @@ public class DemoSiteTest {
 			System.out.println("Failed to load homepage");
 		}
 	}
-	
+
 	@AfterAll
 	public static void teardown() {
 		driver.quit();
 	}
-	
+
 	@Test
-	public void test() {
-		
+	public void test() throws InterruptedException {
+		String username = "username";
+		String password = "password";
+		HomePage nav = PageFactory.initElements(driver, HomePage.class);
+		AddUser user = PageFactory.initElements(driver, AddUser.class);
+
+		nav.navAddUser();
+		user.info(username, password);
+		Thread.sleep(3000);
+		nav.navLogin();
+		user.info(username, password);
+		Thread.sleep(3000);
+
+		assertEquals("**Successful Login**",
+				driver.findElement(By.xpath("//b[contains(text(),'**Successful Login**')]")).getText());
 	}
 }
